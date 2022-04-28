f2cbc38a8b5bb09045d78872568ba7218481b0e2
edoed <edoardo.carlini@mail.polimi.it>
2022-04-28 09:51:52 +0200

    server folders and messages reorganised

--- src/main/java/it/polimi/ingsw/network/server/Server.java
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -1,4 +1,120 @@
 package it.polimi.ingsw.network.server;
 
+import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.network.server.message.Message;
+import it.polimi.ingsw.view.VirtualView;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Main server class that starts a socket server.
+ * It can handle different types of connections.
+ */
 public class Server {
+
+    private final Controller gameController;
+
+    private final Map<String, ClientHandler> clientHandlerMap;
+
+    public static final Logger LOGGER = Logger.getLogger(Server.class.getName());
+
+    private final Object lock;
+
+    public Server(Controller gameController) {
+        this.gameController = gameController;
+        this.clientHandlerMap = Collections.synchronizedMap(new HashMap<>());
+        this.lock = new Object();
+    }
+
+    /**
+     * Adds a client to be managed by the server instance.
+     *
+     * @param nickname      the nickname associated with the client.
+     * @param clientHandler the ClientHandler associated with the client.
+     */
+    public void addClient(String nickname, ClientHandler clientHandler) {
+        VirtualView vv = new VirtualView(clientHandler);
+
+        if (!gameController.isGameStarted()) {
+            if (gameController.checkLoginNickname(nickname, vv)) {
+                clientHandlerMap.put(nickname, clientHandler);
+                gameController.loginHandler(nickname, vv);
+            }
+        } else {
+            vv.showLoginResult(true, false, null);
+            clientHandler.disconnect();
+        }
+
+    }
+
+    /**
+     * Removes a client given his nickname.
+     *
+     * @param nickname      the VirtualView to be removed.
+     * @param notifyEnabled set to {@code true} to enable a lobby disconnection message, {@code false} otherwise.
+     */
+    public void removeClient(String nickname, boolean notifyEnabled) {
+        clientHandlerMap.remove(nickname);
+        gameController.removeVirtualView(nickname, notifyEnabled);
+        LOGGER.info(() -> "Removed " + nickname + " from the client list.");
+    }
+
+    /**
+     * Forwards a received message from the client to the GameController.
+     *
+     * @param message the message to be forwarded.
+     */
+    public void onMessageReceived(Message message) {
+        gameController.onMessageReceived(message);
+    }
+
+    /**
+     * Handles the disconnection of a client.
+     *
+     * @param clientHandler the client disconnecting.
+     */
+    public void onDisconnect(ClientHandler clientHandler) {
+        synchronized (lock) {
+            String nickname = getNicknameFromClientHandler(clientHandler);
+
+            if (nickname != null) {
+
+                boolean gameStarted = gameController.isGameStarted();
+                removeClient(nickname, !gameStarted); // enable lobby notifications only if the game didn't start yet.
+
+                if(gameController.getTurnController() != null &&
+                        !gameController.getTurnController().getNicknameQueue().contains(nickname)) {
+                    return;
+                }
+
+                // Resets server status only if the game was already started.
+                // Otherwise the server will wait for a new player to connect.
+                if (gameStarted) {
+                    gameController.broadcastDisconnectionMessage(nickname, " disconnected from the server. GAME ENDED.");
+
+                    gameController.endGame();
+                    clientHandlerMap.clear();
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Returns the corresponding nickname of a ClientHandler.
+     *
+     * @param clientHandler the client handler.
+     * @return the corresponding nickname of a ClientHandler.
+     */
+    private String getNicknameFromClientHandler(ClientHandler clientHandler) {
+        return clientHandlerMap.entrySet()
+                .stream()
+                .filter(entry -> clientHandler.equals(entry.getValue()))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
 }
---
+++ src/main/java/it/polimi/ingsw/network/server/SocketServer.java
@@ -0,0 +1,74 @@
+package it.polimi.ingsw.network.server;
+
+import it.polimi.ingsw.network.server.message.Message;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Socket server that handles all the new socket connection.
+ */
+public class SocketServer implements Runnable {
+    private final Server server;
+    private final int port;
+    ServerSocket serverSocket;
+
+    public SocketServer(Server server, int port) {
+        this.server = server;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(port);
+            Server.LOGGER.info(() -> "Socket server started on port " + port + ".");
+        } catch (IOException e) {
+            Server.LOGGER.severe("Server could not start!");
+            return;
+        }
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Socket client = serverSocket.accept();
+
+                client.setSoTimeout(5000);
+
+                SocketClientHandler clientHandler = new SocketClientHandler(this, client);
+                Thread thread = new Thread(clientHandler, "ss_handler" + client.getInetAddress());
+                thread.start();
+            } catch (IOException e) {
+                Server.LOGGER.severe("Connection dropped");
+            }
+        }
+    }
+
+    /**
+     * Handles the addition of a new client.
+     *
+     * @param nickname      the nickname of the new client.
+     * @param clientHandler the ClientHandler of the new client.
+     */
+    public void addClient(String nickname, ClientHandler clientHandler) {
+        server.addClient(nickname, clientHandler);
+    }
+
+    /**
+     * Forwards a received message from the client to the Server.
+     *
+     * @param message the message to be forwarded.
+     */
+    public void onMessageReceived(Message message) {
+        server.onMessageReceived(message);
+    }
+
+    /**
+     * Handles a client disconnection.
+     *
+     * @param clientHandler the ClientHandler of the disconnecting client.
+     */
+    public void onDisconnect(ClientHandler clientHandler) {
+        server.onDisconnect(clientHandler);
+    }
+}
