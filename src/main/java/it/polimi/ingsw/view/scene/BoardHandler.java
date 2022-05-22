793a2225d4867bd58aaa7f8b076547699197999b
edocarli96 <10691596i@polimi.it>
2022-05-22 16:58:00 +0200

    main board demo

---
+++ src/main/java/it/polimi/ingsw/view/scene/BoardHandler.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.view.scene;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
+public class BoardHandler extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("playerBoard.fxml"));
+        primaryStage.setTitle("eriantys");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public void main(String[] args) {
+        launch(args);
+    }
+
+
+}
