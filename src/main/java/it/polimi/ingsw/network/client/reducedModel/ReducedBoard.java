2f62048d33768f676bc779090a5bfaac7a83ee85
francescodepaola2 <10676956@polimi.it>
2022-06-30 10:53:33 +0200

    ReducedBoard javadoc

--- src/main/java/it/polimi/ingsw/network/client/reducedModel/ReducedBoard.java
+++ src/main/java/it/polimi/ingsw/network/client/reducedModel/ReducedBoard.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Map;
 
+/** This class is useful for showing client-side game board updates **/
+
 public class ReducedBoard implements Serializable {
     @Serial
     private static final long serialVersionUID = -6983972997025348137L;
@@ -18,6 +20,13 @@ public class ReducedBoard implements Serializable {
     private ArrayList<ReducedIsland> islands;
     private ReducedPlayerBoard playerBoard;
 
+    /**
+     * Default constructor
+     * @param clouds       list of clouds
+     * @param reducedPlayerBoard       lighter version of the player board
+     * @param motherNature        motherNature position
+     * @param islands          list of islands after reduction
+     */
     public ReducedBoard(Cloud[] clouds, String[] owner, ReducedPlayerBoard reducedPlayerBoard, int motherNature, ArrayList<ReducedIsland> islands) {
         this.clouds = clouds;
         this.owner = owner;
@@ -29,13 +38,16 @@ public class ReducedBoard implements Serializable {
         islands.get(motherNature).setMotherNature(true);
     }
 
+
     public Cloud[] getClouds() {
         return clouds;
     }
 
+
     public ArrayList<ReducedIsland> getIslands() {
         return islands;
     }
+
 
     public int getMotherNature() {
         return motherNature;
@@ -45,14 +57,17 @@ public class ReducedBoard implements Serializable {
         this.motherNature = motherNature;
     }
 
+    /** This method removes the student from the island **/
     public void removeStudentFromIsland(int id, Student color){
         islands.get(id).addStudent(color);
     }
 
+    /** This method adds the student to the island **/
     public void addStudentToIsland(int id, Student color){
         islands.get(id).addStudent(color);
     }
 
+    /** This method prints the islands **/
     public String printIslands() {
         String printedIslands = "";
         
@@ -67,6 +82,7 @@ public class ReducedBoard implements Serializable {
         return playerBoard.print();
     }
 
+    /** This method prints the clouds **/
     public String printClouds() {
         String print = "";
         int i = 1;
@@ -80,6 +96,7 @@ public class ReducedBoard implements Serializable {
         return print;
     }
 
+    /** This method moves motherNature **/
     public void moveMotherNature(int steps){
         motherNature += steps;
 
@@ -96,6 +113,7 @@ public class ReducedBoard implements Serializable {
         return playerBoard;
     }
 
+    /** This method merge the islands **/
     public void mergeIslands(int island1, int island2) {
         islands.get(island2).mergeStudents(islands.get(island1).getNumStudents());
 
